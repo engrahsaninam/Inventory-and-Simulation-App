02cf4f1b79db3b2481732cd8fc62fdb500d991bb
Ahsan Inam <83651389+engrahsaninam@users.noreply.github.com>
2023-09-18 15:57:18 +0500

    updated

--- home/views.py
+++ home/views.py
@@ -43,7 +43,7 @@ def index(request):
         # Initialize or load your DataFrame (df1) here
         try:
             if browse is not None:
-                df = pd.read_csv(browse)
+                df = pd.read_excel(browse)
             else:
                 df = pd.DataFrame()  # Initialize an empty DataFrame if no file is uploaded
         except Exception as e:
@@ -52,130 +52,133 @@ def index(request):
             df = pd.DataFrame()  # Initialize an empty DataFrame as a fallback
         
         if not df.empty:
+            try:
+                df = df.dropna(how='all')
+                df = df.fillna(0)
+                df = df.replace({0: pd.NA, '0': pd.NA, '': pd.NA, 'None': pd.NA})
 
-            df = df.dropna(how='all')
-            df = df.fillna(0)
-            df = df.replace({0: pd.NA, '0': pd.NA, '': pd.NA, 'None': pd.NA})
+                start_col = df.columns.get_loc('Description')
 
-            start_col = df.columns.get_loc('Description')
+                end_col = len(df.columns) - 3
+                current_min_col = df.iloc[:, end_col + 1]
+                current_max_col = df.iloc[:, end_col + 2]
 
-            end_col = len(df.columns) - 3
-            current_min_col = df.iloc[:, end_col + 1]
-            current_max_col = df.iloc[:, end_col + 2]
-            
-            # Perform calculations on df1
-            df['Total'] = df.iloc[:, start_col + 1:end_col].apply(opt_filtered_row, axis=1).sum(axis=1)
-            df['LifeTime'] = df.iloc[:, start_col + 1:end_col].apply(opt_length, axis=1)
-            df['Min Order'] = df.iloc[:, start_col + 1:end_col].apply(opt_filtered_row, axis=1).replace(
-                {'0': pd.NA, 0: pd.NA}).min(axis=1)
-            df['Max Order'] = df.iloc[:, start_col + 1:end_col].apply(opt_filtered_row, axis=1).max(axis=1)
-            df['Adjusted LT (days)'] = df.iloc[:, end_col] + LT_Adjustment
-            df['Adjusted LT (month)'] = df['Adjusted LT (days)'] / 30.41666667
-            df['Average Demand'] = df['Total'] / df['LifeTime']
-            df['Demand over LT'] = df['Average Demand'] * df['Adjusted LT (month)']
-            df['Std. Deviation of Demand'] = df.iloc[:, start_col + 1:end_col].apply(opt_nonzerostd, 1)
-            df['Std. Dev. of Demand over LT'] = df['Std. Deviation of Demand'] * np.sqrt(
-                df['Adjusted LT (month)'].astype(float))
-            df['Safety Stock'] = scipy.stats.norm.ppf(Alpha, 0, 1) * df['Std. Dev. of Demand over LT']
-            df['Min'] = scipy.stats.norm.ppf(Alpha, 0, 1) * df['Std. Dev. of Demand over LT'] + df['Demand over LT']
+                # Perform calculations on df1
+                df['Total'] = df.iloc[:, start_col + 1:end_col].apply(opt_filtered_row, axis=1).sum(axis=1)
+                df['LifeTime'] = df.iloc[:, start_col + 1:end_col].apply(opt_length, axis=1)
+                df['Min Order'] = df.iloc[:, start_col + 1:end_col].apply(opt_filtered_row, axis=1).replace(
+                    {'0': pd.NA, 0: pd.NA}).min(axis=1)
+                df['Max Order'] = df.iloc[:, start_col + 1:end_col].apply(opt_filtered_row, axis=1).max(axis=1)
+                df['Adjusted LT (days)'] = df.iloc[:, end_col] + LT_Adjustment
+                df['Adjusted LT (month)'] = df['Adjusted LT (days)'] / 30.41666667
+                df['Average Demand'] = df['Total'] / df['LifeTime']
+                df['Demand over LT'] = df['Average Demand'] * df['Adjusted LT (month)']
+                df['Std. Deviation of Demand'] = df.iloc[:, start_col + 1:end_col].apply(opt_nonzerostd, 1)
+                df['Std. Dev. of Demand over LT'] = df['Std. Deviation of Demand'] * np.sqrt(
+                    df['Adjusted LT (month)'].astype(float))
+                df['Safety Stock'] = scipy.stats.norm.ppf(Alpha, 0, 1) * df['Std. Dev. of Demand over LT']
+                df['Min'] = scipy.stats.norm.ppf(Alpha, 0, 1) * df['Std. Dev. of Demand over LT'] + df['Demand over LT']
 
-            if benchmark == 'No BenchMark':
-                df['Adjusted MIN'] = df['Min']
-            elif benchmark == 'Current Min':
-                df['Adjusted MIN'] = np.where(df['Min'] < current_min_col, current_min_col, df['Min'])
-            elif benchmark == 'Max Order':
-                df['Adjusted MIN'] = np.where(df['Min'] < df['Max Order'], df['Max Order'], df['Min'])
-            elif benchmark == 'Both Current Min & Max Order':
-                max_result = np.where(current_min_col < df['Max Order'], df['Max Order'], current_min_col)
-                df['Adjusted MIN'] = np.where(df['Min'] < max_result, max_result, df['Min'])
-            elif benchmark == 'Slow Moving Items':
-                df['Adjusted MIN'] = np.ceil(
-                    np.where(df['Demand over LT'] > 10, df['Demand over LT'] + df['Average Demand'],
-                             df['Demand over LT'] + 1)).astype(int)
-                df['MAX'] = np.ceil(df['Adjusted MIN'] + df['Demand over LT']).astype(int)
+                if benchmark == 'No BenchMark':
+                    df['Adjusted MIN'] = df['Min']
+                elif benchmark == 'Current Min':
+                    df['Adjusted MIN'] = np.where(df['Min'] < current_min_col, current_min_col, df['Min'])
+                elif benchmark == 'Max Order':
+                    df['Adjusted MIN'] = np.where(df['Min'] < df['Max Order'], df['Max Order'], df['Min'])
+                elif benchmark == 'Both Current Min & Max Order':
+                    max_result = np.where(current_min_col < df['Max Order'], df['Max Order'], current_min_col)
+                    df['Adjusted MIN'] = np.where(df['Min'] < max_result, max_result, df['Min'])
+                elif benchmark == 'Slow Moving Items':
+                    df['Adjusted MIN'] = np.ceil(
+                        np.where(df['Demand over LT'] > 10, df['Demand over LT'] + df['Average Demand'],
+                                 df['Demand over LT'] + 1)).astype(int)
+                    df['MAX'] = np.ceil(df['Adjusted MIN'] + df['Demand over LT']).astype(int)
 
-            elif benchmark == 'Compare All':
-                df['Adjusted MIN-No BenchMark'] = df['Min']
-                df['Adjusted MIN-Current Min'] = np.where(df['Min'] < current_min_col, current_min_col, df['Min'])
-                df['Adjusted MIN-Max Order'] = np.where(df['Min'] < df['Max Order'], df['Max Order'], df['Min'])
-                max_result = np.where(current_min_col < df['Max Order'], df['Max Order'], current_min_col)
-                df['Adjusted MIN-Both Current Min & Max Order'] = np.where(df['Min'] < max_result, max_result, df['Min'])
-                df['Adjusted MIN-Slow Moving Items'] = np.ceil(
-                    np.where(df['Demand over LT'] > 10, df['Demand over LT'] + df['Average Demand'],
-                             df['Demand over LT'] + 1)).astype(int)
+                elif benchmark == 'Compare All':
+                    df['Adjusted MIN-No BenchMark'] = df['Min']
+                    df['Adjusted MIN-Current Min'] = np.where(df['Min'] < current_min_col, current_min_col, df['Min'])
+                    df['Adjusted MIN-Max Order'] = np.where(df['Min'] < df['Max Order'], df['Max Order'], df['Min'])
+                    max_result = np.where(current_min_col < df['Max Order'], df['Max Order'], current_min_col)
+                    df['Adjusted MIN-Both Current Min & Max Order'] = np.where(df['Min'] < max_result, max_result, df['Min'])
+                    df['Adjusted MIN-Slow Moving Items'] = np.ceil(
+                        np.where(df['Demand over LT'] > 10, df['Demand over LT'] + df['Average Demand'],
+                                 df['Demand over LT'] + 1)).astype(int)
 
-                df['MAX-No BenchMark'] = (df['Average Demand'] * ReviewPeriod) + df['Adjusted MIN-No BenchMark']
-                df['MAX-Current Min'] = (df['Average Demand'] * ReviewPeriod) + df['Adjusted MIN-Current Min']
-                df['MAX-Max Order'] = (df['Average Demand'] * ReviewPeriod) + df['Adjusted MIN-Max Order']
-                df['MAX-Both Current Min & Max Order'] = (df['Average Demand'] * ReviewPeriod) + df['Adjusted MIN-Both']
-                df['MAX-Slow Moving Items'] = np.ceil(df['Adjusted MIN-Slow Moving Items'] + df['Demand over LT']).astype(int)
+                    df['MAX-No BenchMark'] = (df['Average Demand'] * ReviewPeriod) + df['Adjusted MIN-No BenchMark']
+                    df['MAX-Current Min'] = (df['Average Demand'] * ReviewPeriod) + df['Adjusted MIN-Current Min']
+                    df['MAX-Max Order'] = (df['Average Demand'] * ReviewPeriod) + df['Adjusted MIN-Max Order']
+                    df['MAX-Both Current Min & Max Order'] = (df['Average Demand'] * ReviewPeriod) + df['Adjusted MIN-Both']
+                    df['MAX-Slow Moving Items'] = np.ceil(df['Adjusted MIN-Slow Moving Items'] + df['Demand over LT']).astype(int)
 
-            if benchmark != 'Slow Moving Items' and benchmark != 'Compare All':
-                df['MAX'] = (df['Average Demand'] * ReviewPeriod) + df['Adjusted MIN']
+                if benchmark != 'Slow Moving Items' and benchmark != 'Compare All':
+                    df['MAX'] = (df['Average Demand'] * ReviewPeriod) + df['Adjusted MIN']
 
-            if benchmark == 'Compare All':
-                df['Cycle Service Level-No BenchMark'] = scipy.stats.norm.cdf(
-                    (df['Adjusted MIN-No BenchMark'].astype(float) + 0.0056).round(decimals=4),
-                    df['Demand over LT'].astype(float).round(decimals=3),
-                    df['Std. Dev. of Demand over LT'].replace({0: 0.001}).astype(float).round(decimals=4))
+                if benchmark == 'Compare All':
+                    df['Cycle Service Level-No BenchMark'] = scipy.stats.norm.cdf(
+                        (df['Adjusted MIN-No BenchMark'].astype(float) + 0.0056).round(decimals=4),
+                        df['Demand over LT'].astype(float).round(decimals=3),
+                        df['Std. Dev. of Demand over LT'].replace({0: 0.001}).astype(float).round(decimals=4))
 
-                df['Cycle Service Level-Current Min'] = scipy.stats.norm.cdf(
-                    (df['Adjusted MIN-Current Min'].astype(float) + 0.0056).round(decimals=4),
-                    df['Demand over LT'].astype(float).round(decimals=3),
-                    df['Std. Dev. of Demand over LT'].replace({0: 0.001}).astype(float).round(decimals=4))
+                    df['Cycle Service Level-Current Min'] = scipy.stats.norm.cdf(
+                        (df['Adjusted MIN-Current Min'].astype(float) + 0.0056).round(decimals=4),
+                        df['Demand over LT'].astype(float).round(decimals=3),
+                        df['Std. Dev. of Demand over LT'].replace({0: 0.001}).astype(float).round(decimals=4))
 
-                df['Cycle Service Level-Max Order'] = scipy.stats.norm.cdf(
-                    (df['Adjusted MIN-Max Order'].astype(float) + 0.0056).round(decimals=4),
-                    df['Demand over LT'].astype(float).round(decimals=3),
-                    df['Std. Dev. of Demand over LT'].replace({0: 0.001}).astype(float).round(decimals=4))
+                    df['Cycle Service Level-Max Order'] = scipy.stats.norm.cdf(
+                        (df['Adjusted MIN-Max Order'].astype(float) + 0.0056).round(decimals=4),
+                        df['Demand over LT'].astype(float).round(decimals=3),
+                        df['Std. Dev. of Demand over LT'].replace({0: 0.001}).astype(float).round(decimals=4))
 
-                df['Cycle Service Level-Both Current Min & Max Order'] = scipy.stats.norm.cdf(
-                    (df['Adjusted MIN-Both Current Min & Max Order'].astype(float) + 0.0056).round(decimals=4),
-                    df['Demand over LT'].astype(float).round(decimals=3),
-                    df['Std. Dev. of Demand over LT'].replace({0: 0.001}).astype(float).round(decimals=4))
+                    df['Cycle Service Level-Both Current Min & Max Order'] = scipy.stats.norm.cdf(
+                        (df['Adjusted MIN-Both Current Min & Max Order'].astype(float) + 0.0056).round(decimals=4),
+                        df['Demand over LT'].astype(float).round(decimals=3),
+                        df['Std. Dev. of Demand over LT'].replace({0: 0.001}).astype(float).round(decimals=4))
 
-                df['Cycle Service Level-Slow Moving Items'] = scipy.stats.norm.cdf(
-                    (df['Adjusted MIN-Slow Moving Items'].astype(float) + 0.0056).round(decimals=4),
-                    df['Demand over LT'].astype(float).round(decimals=3),
-                    df['Std. Dev. of Demand over LT'].replace({0: 0.001}).astype(float).round(decimals=4))
-            else:
-                df['Cycle Service Level'] = scipy.stats.norm.cdf(
-                    (df['Adjusted MIN'].astype(float) + 0.0056).round(decimals=4),
-                    df['Demand over LT'].astype(float).round(decimals=3),
-                    df['Std. Dev. of Demand over LT'].replace({0: 0.001}).astype(float).round(decimals=4))
+                    df['Cycle Service Level-Slow Moving Items'] = scipy.stats.norm.cdf(
+                        (df['Adjusted MIN-Slow Moving Items'].astype(float) + 0.0056).round(decimals=4),
+                        df['Demand over LT'].astype(float).round(decimals=3),
+                        df['Std. Dev. of Demand over LT'].replace({0: 0.001}).astype(float).round(decimals=4))
+                else:
+                    df['Cycle Service Level'] = scipy.stats.norm.cdf(
+                        (df['Adjusted MIN'].astype(float) + 0.0056).round(decimals=4),
+                        df['Demand over LT'].astype(float).round(decimals=3),
+                        df['Std. Dev. of Demand over LT'].replace({0: 0.001}).astype(float).round(decimals=4))
 
-            df['Adjusted LT (month)'] = df['Adjusted LT (month)'].astype(float).round(1)
-            df['Average Demand'] = df['Average Demand'].astype(float).round(1)
-            df['Demand over LT'] = df['Demand over LT'].astype(float).round(1)
-            df['Std. Deviation of Demand'] = df['Std. Deviation of Demand'].astype(float).round(1)
-            df['Std. Dev. of Demand over LT'] = df['Std. Dev. of Demand over LT'].astype(float).round(1)
-            df['Safety Stock'] = df['Safety Stock'].astype(float).round(1)
-            df['Min'] = df['Min'].astype(float).round(1)
+                df['Adjusted LT (month)'] = df['Adjusted LT (month)'].astype(float).round(1)
+                df['Average Demand'] = df['Average Demand'].astype(float).round(1)
+                df['Demand over LT'] = df['Demand over LT'].astype(float).round(1)
+                df['Std. Deviation of Demand'] = df['Std. Deviation of Demand'].astype(float).round(1)
+                df['Std. Dev. of Demand over LT'] = df['Std. Dev. of Demand over LT'].astype(float).round(1)
+                df['Safety Stock'] = df['Safety Stock'].astype(float).round(1)
+                df['Min'] = df['Min'].astype(float).round(1)
 
-            if benchmark == 'Compare All':
-                for item in ['No BenchMark', 'Current Min', 'Max Order', 'Both Current Min & Max Order', 'Slow Moving Items']:
-                    df['Adjusted MIN-{}'.format(item)] = df['Adjusted MIN-{}'.format(item)].astype(float).round(1)
-                    df['MAX-{}'.format(item)] = df['MAX-{}'.format(item)].astype(float).round(1)
-                    df['Cycle Service Level-{}'.format(item)] = df['Cycle Service Level-{}'.format(item)].astype(
-                        float).round(5)
-            else:
-                df['Adjusted MIN'] = df['Adjusted MIN'].astype(float).round(1)
-                df['MAX'] = df['MAX'].astype(float).round(1)
-                df['Cycle Service Level'] = df['Cycle Service Level'].astype(float).round(5)
+                if benchmark == 'Compare All':
+                    for item in ['No BenchMark', 'Current Min', 'Max Order', 'Both Current Min & Max Order', 'Slow Moving Items']:
+                        df['Adjusted MIN-{}'.format(item)] = df['Adjusted MIN-{}'.format(item)].astype(float).round(1)
+                        df['MAX-{}'.format(item)] = df['MAX-{}'.format(item)].astype(float).round(1)
+                        df['Cycle Service Level-{}'.format(item)] = df['Cycle Service Level-{}'.format(item)].astype(
+                            float).round(5)
+                else:
+                    df['Adjusted MIN'] = df['Adjusted MIN'].astype(float).round(1)
+                    df['MAX'] = df['MAX'].astype(float).round(1)
+                    df['Cycle Service Level'] = df['Cycle Service Level'].astype(float).round(5)
 
-            # Prepare data for rendering
-            context = {
-                'result_data': {
-                    'LT_Adjustment': LT_Adjustment,
-                    'Alpha': Alpha,
-                    'ReviewPeriod': ReviewPeriod,
-                    'benchmark': benchmark,
-                    # Include other calculated values here
-                },
-                'df1_results': df.to_dict(orient='records')
-            }
+                # Prepare data for rendering
+                context = {
+                    'result_data': {
+                        'LT_Adjustment': LT_Adjustment,
+                        'Alpha': Alpha,
+                        'ReviewPeriod': ReviewPeriod,
+                        'benchmark': benchmark,
+                        # Include other calculated values here
+                    },
+                    'df1_results': df.to_dict(orient='records')
+                }
 
-            return render(request, 'index.html', context)
+                return render(request, 'index.html', context)
+            except Exception as e:
+                # Handle the case where the DataFrame is empty or couldn't be loaded
+                return render(request, 'index.html', {'error_message': 'Error Alert '+str(e)})
         else:
             # Handle the case where the DataFrame is empty or couldn't be loaded
             return render(request, 'index.html', {'error_message': 'Error loading or processing the file'})
